c3f15343c1190c8bd9fe1adcdaa05949e75ff8ea
Max Ladner <ladnerm78@gmail.com>
2025-06-01 19:28:01 -0400

    idk just some stuff

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -3,19 +3,19 @@ package handlers
 import (
 	"encoding/json"
 	"fmt"
+	"grocery-list/templates"
 	"grocery-list/types"
 	"net/http"
 	"os"
-    "strconv"
-    "grocery-list/templates"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
 func HandlerGetIndex() func(*gin.Context) {
 	return func(c *gin.Context) {
-        c.Status(http.StatusOK)
-        templates.Temp.Execute(c.Writer, nil)
+		c.Status(http.StatusOK)
+		templates.Temp.Execute(c.Writer, nil)
 	}
 }
 
@@ -57,9 +57,9 @@ func HandlerPostForm() func(*gin.Context) {
 			//do something
 		}
 
-        if len(itemArr) != 0 {
-            item.ID = itemArr[len(itemArr)-1].ID + 1
-        }
+		if len(itemArr) != 0 {
+			item.ID = itemArr[len(itemArr)-1].ID + 1
+		}
 		itemArr = append(itemArr, *item)
 		db.Close()
 
@@ -79,8 +79,8 @@ func HandlerPostForm() func(*gin.Context) {
 }
 
 func HandlerDeleteItem() func(*gin.Context) {
-    return func(c *gin.Context) {
-        id := c.Param("id")
+	return func(c *gin.Context) {
+		id := c.Param("id")
 
 		db, dbErr := os.OpenFile("db/db.json", os.O_RDWR, 0644)
 		if dbErr != nil {
@@ -95,18 +95,17 @@ func HandlerDeleteItem() func(*gin.Context) {
 		}
 		db.Close()
 
-        var newItemArr []types.Item
+		var newItemArr []types.Item
 
-        for _, v := range itemArr {
-            iid, err := strconv.Atoi(id)
-            if err != nil {
-                //Do something
-            }
-            if v.ID != iid {
-                newItemArr = append(newItemArr, v)
-            }
-        }
-
+		for _, v := range itemArr {
+			iid, err := strconv.Atoi(id)
+			if err != nil {
+				//Do something
+			}
+			if v.ID != iid {
+				newItemArr = append(newItemArr, v)
+			}
+		}
 
 		db, dbErr = os.Create("db/db.json")
 		defer db.Close()
@@ -114,14 +113,10 @@ func HandlerDeleteItem() func(*gin.Context) {
 			fmt.Printf("DB ERROR! %v", dbErr)
 		}
 		encdr := json.NewEncoder(db)
-        err = encdr.Encode(newItemArr); 
+		err = encdr.Encode(newItemArr)
 		if err != nil {
 			fmt.Print(err)
 		}
 
-    }
+	}
 }
-
-
-
-
